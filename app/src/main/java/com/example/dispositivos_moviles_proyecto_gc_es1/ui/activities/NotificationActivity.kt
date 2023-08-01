b6f9522c710a83e9ff43dcf04a59a5c4306ad429
easolano98 <solano_erick@hotmail.com>
2023-08-01 00:11:47 -0500

    notificacion con alarma

--- app/src/main/java/com/example/dispositivos_moviles_proyecto_gc_es1/ui/activities/NotificationActivity.kt
+++ app/src/main/java/com/example/dispositivos_moviles_proyecto_gc_es1/ui/activities/NotificationActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dispositivos_moviles_proyecto_gc_es1.ui.activities
 
 import android.annotation.SuppressLint
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -9,27 +10,45 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.dispositivos_moviles_proyecto_gc_es1.R
 import com.example.dispositivos_moviles_proyecto_gc_es1.databinding.ActivityNotificationBinding
+import com.example.dispositivos_moviles_proyecto_gc_es1.ui.utilities.BroadcasterNotifications
+import java.util.Calendar
 
 class NotificationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNotificationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityNotificationBinding.inflate(layoutInflater)
+        binding = ActivityNotificationBinding.inflate(layoutInflater)
         //Se debe ubicar en un lugar donde se cree una sola vez
         //Just once
-        //createNotificationChannel()
-        binding.btnNotification.setOnClickListener {
+        createNotificationChannel()
 
+        setContentView(binding.root)
+
+        binding.btnNotification.setOnClickListener {
+            //createNotificationChannel()
             sendNotification()
         }
-        setContentView(binding.root)
+        binding.btnNotificationProgramada.setOnClickListener {
+            val calendar= Calendar.getInstance()
+            val hora= binding.timepicker.hour
+            val minutes=binding.timepicker.minute
+
+            Toast.makeText(this, "La notificacion se activa a las: $hora con $minutes", Toast.LENGTH_LONG).show()
+            calendar.set(Calendar.HOUR, hora)
+            calendar.set(Calendar.MINUTE, minutes)
+            calendar.set(Calendar.SECOND, 0)
+
+            sendNotificationTimePicker(calendar.timeInMillis)
+        }
     }
+
     //Construccion externa del canal
-    val CHANNEL:String ="Notificacion 1"
+    val CHANNEL: String = "Notificacion 1"
 
     //Implementar el pending y flag
 
@@ -37,7 +56,7 @@ class NotificationActivity : AppCompatActivity() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Canal principal"
+            val name = "Canal principal1"
             val descriptionText = "Canal de notificaciones de variedades"
             val importance = NotificationManager.IMPORTANCE_HIGH
             //(ID DEL CANAL,name,importance)
@@ -50,10 +69,25 @@ class NotificationActivity : AppCompatActivity() {
             notificationManager.createNotificationChannel(channel)
         }
     }
+    //Las echas se mandan como long
+    private fun sendNotificationTimePicker(time: Long) {
+        val myIntent = Intent(applicationContext, BroadcasterNotifications::class.java)
+        //No va a hacer que cuando este abierta actualice la aplicacion a la pantalla, y si es que no esta abierta que abra una nueva (Banderas)
+        val myPendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            0,
+            myIntent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager=getSystemService(Context.ALARM_SERVICE)as AlarmManager
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, time, myPendingIntent)
+
+    }
 
     @SuppressLint("MissingPermission")
-     fun sendNotification() {
-         //Contruccion internar de la notificacion
+    fun sendNotification() {
+        //Contruccion internar de la notificacion
         val intent = Intent(this, CameraActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
@@ -72,10 +106,13 @@ class NotificationActivity : AppCompatActivity() {
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setContentIntent(pendingIntent)
             .setAutoCancel(true)
-            .setStyle(NotificationCompat.BigTextStyle().bigText("Esta es una notificacion para recordar que estamos trabajando con Android")) // Este texto se muestra cuando se abre el gestor de notificaciones
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Esta es una notificacion para recordar que estamos trabajando con Android")
+            ) // Este texto se muestra cuando se abre el gestor de notificaciones
 
         // Aqui se envia la notificacion
-        with(NotificationManagerCompat.from(this)){
+        with(NotificationManagerCompat.from(this)) {
             notify(1, noti.build())
         }
     }
---
+++ app/src/main/java/com/example/dispositivos_moviles_proyecto_gc_es1/ui/utilities/BroadcasterNotifications.kt
@@ -0,0 +1,53 @@
+package com.example.dispositivos_moviles_proyecto_gc_es1.ui.utilities
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.dispositivos_moviles_proyecto_gc_es1.R
+import com.example.dispositivos_moviles_proyecto_gc_es1.ui.activities.NotificationActivity
+
+class BroadcasterNotifications: BroadcastReceiver() {
+    private val CHANNEL:String="Notificacion 1"
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+
+        val myIntent=Intent(context, NotificationActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            myIntent,
+            PendingIntent.FLAG_MUTABLE
+        )
+        val notif = NotificationCompat.Builder(context, CHANNEL)
+        notif.setContentTitle("Broadcaster notificacion mi llave :v")
+        notif.setContentText("Abre tu wea :v")
+        notif.setSmallIcon(R.drawable.marvel_icon)
+        notif.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        notif.setContentIntent(pendingIntent)
+        notif.setAutoCancel(true)
+
+        notif.setStyle(
+            NotificationCompat.BigTextStyle()
+                .bigText("alo")
+        )
+
+        //crear un manager de tipo broadcast
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(
+            1,
+            notif.build()
+        )
+
+
+    }
+
+
+
+
+}
